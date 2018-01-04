7dc934ae846d366ca35e85eae9e8b4b56666c222
Shane Richards <shane.richards121@gmail.com>
2018-01-03 19:10:43 -0500

    added import for firebasetoken

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use JWTAuth;
+use App\Models\FirebaseToken;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
